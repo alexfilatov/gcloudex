f1b7d8c116f99fc408566500da1afc60bfecacd7
Alex Bird <alexebird@gmail.com>
2017-03-25 15:38:12 -0700

    remove duplicate auth lines

--- lib/auth.ex
+++ lib/auth.ex
@@ -22,10 +22,6 @@ defmodule GCloudex.Auth do
   @compute_read_only          "https://www.googleapis.com/auth/compute.readonly"
   @compute                    "https://www.googleapis.com/auth/compute"  
 
-  # Cloud Speech scopes
-  @compute_read_only          "https://www.googleapis.com/auth/compute.readonly"
-  @compute                    "https://www.googleapis.com/auth/compute"  
-
   @doc """
   Retrieves an authentication token for the Google Cloud Storage service.
   """
@@ -53,7 +49,7 @@ defmodule GCloudex.Auth do
         get_token_response |> Map.get(:token)
 
       :sql_admin ->
-        {:ok, get_token_response} = 
+        {:ok, get_token_response} =
           GoogleAuth.for_scope @sql_scope_admin
 
         get_token_response |> Map.get(:token)
@@ -70,13 +66,13 @@ defmodule GCloudex.Auth do
         get_token_response |> Map.get(:token)
 
       :compute_read_only ->
-        {:ok, get_token_response} = 
+        {:ok, get_token_response} =
           GoogleAuth.for_scope @compute_read_only
 
         get_token_response |> Map.get(:token)
 
       :compute ->
-        {:ok, get_token_response} = 
+        {:ok, get_token_response} =
           GoogleAuth.for_scope @compute
 
         get_token_response |> Map.get(:token)
