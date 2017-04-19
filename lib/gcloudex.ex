f68f08b41d6dbfd7b96a84d67eee92e717be65ba
Alex Filatov <alex@alexfilatov.com>
2017-04-19 11:14:35 +0300

    added possibility to read JSON credentials filename from ENV var

--- lib/gcloudex.ex
+++ lib/gcloudex.ex
@@ -10,8 +10,15 @@ defmodule GCloudex do
   @spec get_project_id :: binary
   def get_project_id do
     :goth
-    |> Application.get_env(:json) 
-    |> Poison.decode! 
+    |> Application.get_env(:json)
+    |> (fn(env_var) ->
+      case env_var do
+        {:system, "GOOGLE_APPLICATION_CREDENTIALS"} -> System.get_env("GOOGLE_APPLICATION_CREDENTIALS") |> File.read!()
+        {:system, var} -> System.get_env(var) # when we have JSON credentials contents in the var
+        json -> json
+      end
+    end).()
+    |> Poison.decode!
     |> Map.get("project_id")
   end
 end
